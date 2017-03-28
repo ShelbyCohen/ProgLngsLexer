cf8a004986a20d5335613c138ca9be84cefaac82
Shelby Cohen <s.cohen131@gmail.com>
2017-03-28 12:56:14 -0400

    Defining token lexeme list of tuples and reading from file

--- src/main/scala/grammar/Main.scala
+++ src/main/scala/grammar/Main.scala
@@ -1,12 +1,38 @@
 package grammar
 
+import scala.io.Source
+
 /**
   * Created by ShelbyCohen on 3/27/17.
   */
 object Main {
-  import grammar.Lexer.defineTokenLexemePairs
-  println(defineTokenLexemePairs())
-  // getMapValue("IDENT")  // "Value found: IDENT"
-  // getMapValue("error")  // "No value found"
-}
+  def main(args: Array[String]) {
+    // println(defineTokenLexemePairs())
 
+    val file = "/Users/ShelbyCohen/Scala/grammar/src/sentences.txt"
+    var tokens: List[String] = List()
+
+    for (token <- Source.fromFile(file).getLines) {
+      tokens = tokens :+ (token)
+      print(token)
+    }
+
+    println(getMapValue("IDENT"))  // "Value found: IDENT"
+    println(getMapValue("error"))  // "No value found"
+  }
+
+  val myMap = Map("END" -> ".", "IDENT" -> "myRobot", "test" -> 0)
+
+    //there is no concept of null pointer
+    def getMapValue(s: String): String = {
+      myMap get s match {
+        case Some(nb) => "Value found: " + nb
+        case None => "No value found"
+      }
+    }
+//    def getMapValue2(s: String): String =
+//      val myMap = Map("IDENT" -> "myRobot", "test" -> 0)
+//
+//      myMap.get(s).map("Value found: " + _).getOrElse("No value found")
+
+}
