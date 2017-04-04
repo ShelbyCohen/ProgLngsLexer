108bcda98bfd6cf45a72096cdb4139c17caa405b
Shelby Cohen <s.cohen131@gmail.com>
2017-04-03 20:03:25 -0400

    adding token&lexeme to new Map and if token is not already defined, then add

--- src/main/scala/grammar/Main.scala
+++ src/main/scala/grammar/Main.scala
@@ -9,25 +9,37 @@ object Main {
   def main(args: Array[String]) {
     val fileContents = ". . myRobot ." toString()
 
-    var lexemes: List[String] = List()
-    lexemes = fileContents.split(" ").toList
+    var tokens: List[String] = List()
+    tokens = fileContents.split(" ").toList
 
-    println(sentence(lexemes.head, lexemes))
+    println(sentence(tokens))
 
   }
 
   val myMap = Map("." -> "END", "myRobot" -> "IDENT")
+  // val myMap = Map("END" -> ".", "IDENT" -> "myRobot")
 
-  def sentence(lexeme: String, lexemes: List[String]): List[String] = {
+  def sentence(tokens: List[String]): Map[String, String] = {
     // val file = "/Users/ShelbyCohen/Scala/grammar/src/sentences.txt"
     // val fileContents = Source.fromFile(file).getLines.mkString
     // lexemes = fileContents.split(" ")
+    var lexemes: List[String] = List()
+    var newMap = Map("test" -> "new")
 
-    var tokens: List[String] = List()
-
-    if (isMapValue(lexemes.head))
-      tokens = getMapValue(lexemes.head) :: tokens
-    else sentence(lexemes.head, lexemes.tail)
+    if (!tokens.isEmpty) {
+      //if (myMap.valuesIterator.exists(_.contains(tokens.head)))
+      if (isMapValue(tokens.head)) {
+        println(tokens.head, getMapValue(tokens.head))
+        // lexemes = tokens.head :: lexemes
+        newMap += (tokens.head -> getMapValue(tokens.head))
+      }
+      else {
+        newMap += (tokens.head -> "ERROR")
+        println(newMap.get(tokens.head))
+        sentence(tokens.tail)
+      }
+    }
+    else newMap
   }
 
   //there is no concept of null pointer
