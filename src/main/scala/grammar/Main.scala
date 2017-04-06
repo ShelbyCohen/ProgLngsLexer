08ad4aec66be08b809e177e7315848d789f2037f
Shelby Cohen <s.cohen131@gmail.com>
2017-04-06 12:35:05 -0400

    Using tuples and lexing through chars with new design

--- src/main/scala/grammar/Main.scala
+++ src/main/scala/grammar/Main.scala
@@ -9,63 +9,55 @@ object Main {
   def main(args: Array[String]) {
     val statement = "set myRobot jump ." toString()
 
+    // val file = "/Users/ShelbyCohen/Scala/grammar/src/sentences.txt"
+    // val fileContents = Source.fromFile(file).getLines.mkString
     // var tokens: List[String] = List()
     // tokens = fileContents.split(" ").toList
-
     println(totalLex(statement))
-
   }
-  val myMap = Map("." -> "END",
-                  "myRobot" -> "IDENT",
-                  "set" -> "Set",
-                  "jump" -> "ACTION")
+
+  val myTuple = List(("." -> "END"),
+    ("myRobot" -> "IDENT"), ("set" -> "Set"), ("jump" -> "ACTION"))
 
   // calls nextLex
   // starts with string and ends with a list of tuples
-  def totalLex(statement: String): ((String, String), (String, String), (String, String) = {
+  def totalLex(inputStr: String): ((String, String), (String, String), (String, String)) = {
 
+    newListofTuples = List(String, String)
+    // add whatever nextLex returns to new list of tuples
+    if(!inputStr.isEmpty)
+      newListofTuples += nextLex(inputStr, index)
+    // return entire list of tuples
+    else newListofTuples
 
   }
 
-  def nextLex(s: String): String = {
-
+  def nextLex(inputStr: String, index: Int): (String, String) = {
+    if(inputStr.charAt(index).isLetter) ident(inputStr, index)
+    if(inputStr.charAt(index).isDigit) intLiteral(inputStr, index)
+    if(!inputStr.charAt(index).isLetterOrDigit) symbols(inputStr, index)
   }
 
-  def lookup(tokens: List[String]): Map[String, String] = {
-    // val file = "/Users/ShelbyCohen/Scala/grammar/src/sentences.txt"
-    // val fileContents = Source.fromFile(file).getLines.mkString
-    // lexemes = fileContents.split(" ")
-    var lexemes: List[String] = List()
-    var newMap = Map("test" -> "new")
-
-    if (!tokens.isEmpty) {
-      //if (myMap.valuesIterator.exists(_.contains(tokens.head)))
-      if (isMapValue(tokens.head)) {
-        println(tokens.head, getMapValue(tokens.head))
-        // lexemes = tokens.head :: lexemes
-        newMap += (tokens.head -> getMapValue(tokens.head))
-      }
-      else {
-        newMap += (tokens.head -> "ERROR")
-        println(newMap.get(tokens.head))
-        lookup(tokens.tail)
-      }
-    }
-    else newMap
+  // checks if lexeme is mapped to a token
+  def lookup(lexeme: String): String = {
+    if (lexeme.isEmpty) return "Error, token is empty"
+    if (isMapValue(lexeme)) getMapValue(lexeme)
+    else return "Error, lexeme is invalid"
   }
 
   // ident takes a String lexeme and returns a lexeme, token, and the rest of the statement
   // assume first char is a letter
   def ident(lexeme: String, index: Int): (String, String, String) = {
-
     token = lookup(lexeme)
 
+    if(lexeme.isEmpty) return (lexeme, token, theRest)
+    if(!lexeme.charAt(index).isLetter) return (lexeme, token, theRest)
+    else ident(lexeme.getChars(index, index++), index++)
   }
 
   // intLiteral takes a String lexeme and returns a lexeme, and the rest of the statement
   // assume first char is a number
   def intLiteral(lexeme: String, index: Int): (String, String, String) = {
-
 
   }
 
@@ -77,14 +69,14 @@ object Main {
 
   //there is no concept of null pointer
   def isMapValue(s: String): Boolean = {
-    myMap get s match {
+    myTuple get s match {
       case Some(nb) => true
       case None => false
     }
   }
 
   def getMapValue(s: String): String = {
-    myMap get s match {
+    myTuple get s match {
       case Some(nb) => nb
       case None => "No value found"
     }
