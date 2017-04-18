4f7f536995d2fe96e99db7c444d5afd6bc3c0c99
Shelby Cohen <s.cohen131@gmail.com>
2017-04-17 22:38:50 -0400

    Adding more tests

--- src/test/scala-2.12/grammar/LexerTest.java
+++ src/test/scala-2.12/grammar/LexerTest.java
@@ -1,7 +1,10 @@
 package grammar;
 
 import org.junit.Test;
+import scala.Tuple2;
 import scala.Tuple3;
+
+import java.util.List;
 
 import static org.junit.Assert.*;
 
@@ -24,48 +27,49 @@ public class LexerTest {
     }
 
     @Test
-    public void testIntLiteral() throws Exception {
+    public void testIdent() throws Exception {
+        Tuple3<String, String, String> identTuple = testClass.ident("set .", 0);
 
+        assertEquals("set", identTuple._1());
+        assertEquals("Set", identTuple._2());
+        assertEquals(".", identTuple._3());
+    }
+
+    @Test
+    public void testIntLiteral() throws Exception {
+        Tuple3<String, String, String> intTuple = testClass.intLiteral("2 2 2", 0);
+
+        assertEquals("2", intTuple._1());
+        assertEquals("INTLITERAL", intTuple._2());
+        assertEquals("2 2", intTuple._3());
     }
 
     @Test
     public void testSymbols() throws Exception {
+        Tuple3<String, String, String> symbolTuple = testClass.symbols(". . 2 test", 0);
 
+        assertEquals(".", symbolTuple._1());
+        assertEquals("END", symbolTuple._2());
+        assertEquals("2 test", symbolTuple._3());
     }
 
     @Test
     public void testTotalLex() throws Exception {
+        // List<Tuple2<String, String>> listOfTuples = testClass.totalLex("move right");
 
+        // assertEquals("move", identTuple._1());
+        // assertEquals("ACTION", identTuple._2());
+        // assertEquals("", identTuple._3());
 
     }
 
     @Test
     public void testNextLex() throws Exception {
 
-    }
+        Tuple3<String, String, String> identTuple = testClass.nextLex("set myRobot right .");
 
-    @Test
-    public void testIdent() throws Exception {
-        String lexeme = "";
-        String token = "";
-        String theRest = "";
-
-        // Tuple3(lexeme, token, theRest) = testClass.ident("", 0);
-
-        testClass.ident("String", 0);
-
-        assertEquals("END", token);
-         //
-
-    }
-
-    @Test
-    public void testGetMapValue() throws Exception {
-
-    }
-
-    @Test
-    public void testIsMapValue() throws Exception {
-
+        assertEquals("set", identTuple._1());
+        assertEquals("Set", identTuple._2());
+        assertEquals("myRobot right .", identTuple._3());
     }
 }
